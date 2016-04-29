e4ae5ccb2e1be21fd9b3340a755c945d52787b1d
zhiguangwang <wangzhiguang@kingsoft.com>
2016-04-29 08:44:32 +0800

    Add some basic packages.

--- provisioners/shell/packages.sh
+++ provisioners/shell/packages.sh
@@ -1,7 +1,30 @@
-#!/bin/bash -ex
+#!/bin/bash
+
+set -e
 
 # Do software and security update
 sudo apt-get update && sudo apt-get upgrade -y
 
+DEPS="
+    apt-show-versions
+    ntp
+    unzip
+    unrar
+    htop
+    ifstat
+    realpath
+    tree
+    jq
+    aria2
+    ruby2.0
+    "
+
 # Install basic packages
-sudo apt-get -y install ntp unzip apt-show-versions ruby2.0
+for DEP in $DEPS; do
+    if ! dpkg -s $DEP > /dev/null 2>&1; then
+    echo "Attempting installation of missing package: $DEP"
+    set -x
+    sudo apt-get install -y $DEP
+    set +x
+    fi
+done
