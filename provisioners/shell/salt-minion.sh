ad818e397f729768cfaf729f1a77f378fd6b0b1d
luyue0821 <luyue1@kingsoft.com>
2017-10-26 18:37:26 +0800

    add version check before installing salt-minion

--- provisioners/shell/salt-minion.sh
+++ provisioners/shell/salt-minion.sh
@@ -7,9 +7,22 @@ SALT_REPO_CONFIG_FILE=/etc/apt/sources.list.d/saltstack.list
 
 # echo 'Installing salt ...'
 
-wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+OS_VERSION=$(lsb_release -r -s)
+
+if [ "$OS_VERSION" == "16.04" ]; then
+    REPO_KEY='https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub'
+    REPO_SOURCE='deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main'
+elif [ "$OS_VERSION" == "14.04" ]; then
+    REPO_KEY='https://repo.saltstack.com/apt/ubuntu/14.04/amd64/latest/SALTSTACK-GPG-KEY.pub'
+    REPO_SOURCE='deb http://repo.saltstack.com/apt/ubuntu/14.04/amd64/latest trusty main'
+else
+    echo "OS version not supported. Currently only support 14.04 and 16.04. Version: $OS_VERSION"
+    exit 1
+fi
+
+wget -O - "$REPO_KEY" | sudo apt-key add -
 sudo touch "$SALT_REPO_CONFIG_FILE"
-echo 'deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main' | sudo tee "$SALT_REPO_CONFIG_FILE"
+echo "$REPO_SOURCE" | sudo tee "$SALT_REPO_CONFIG_FILE"
 sudo apt-get update
 
 # curl -o bootstrap-salt.sh -L https://bootstrap.saltstack.com
