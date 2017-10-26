938d76d6b8168d1d68ee21af6a7cc2ad51f79ab4
luyue0821 <luyue1@kingsoft.com>
2017-10-26 17:27:42 +0800

    add salt repository config before install

--- provisioners/shell/salt-minion.sh
+++ provisioners/shell/salt-minion.sh
@@ -1,12 +1,19 @@
 #!/bin/bash -ex
 
-MASTER=$1
+MASTER_ADDR=$1
 MASTER_CONFIG_FILE=/etc/salt/minion.d/master.conf
 
+SALT_REPO_CONFIG_FILE=/etc/apt/sources.list.d/saltstack.list
+
 # echo 'Installing salt ...'
+
+wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+sudo touch "$SALT_REPO_CONFIG_FILE"
+echo 'deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main' | sudo tee "$SALT_REPO_CONFIG_FILE"
+sudo apt-get update
 
 # curl -o bootstrap-salt.sh -L https://bootstrap.saltstack.com
 # sudo sh bootstrap-salt.sh git develop
 sudo apt-get install salt-minion -y
 sudo touch "$MASTER_CONFIG_FILE"
-echo "master: $MASTER" | sudo tee "$MASTER_CONFIG_FILE"
+echo "master: $MASTER_ADDR" | sudo tee "$MASTER_CONFIG_FILE"
