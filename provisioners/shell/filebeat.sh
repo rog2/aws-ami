192fba57cd589fdbf6467fc776e983a4c6a221f6
zhugongbo <zhugongbo@kingsoft.com>
2017-11-09 19:21:55 +0800

    refine some log info

--- provisioners/shell/filebeat.sh
+++ provisioners/shell/filebeat.sh
@@ -17,16 +17,11 @@ sudo apt-get install apt-transport-https
 #Save the repository definition to /etc/apt/sources.list.d/elastic-5.x.list
 echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
 
+sudo apt-get update
 sudo apt-get install -y filebeat
 
 sudo systemctl start filebeat
 sudo systemctl enable filebeat
 
 sudo systemctl status filebeat
-if [ $? -eq 0 ]; then
-    echo "filebeat is installed successfully"
-    exit 0
-else
-    echo "Error found"
-    exit 1
-fi
+exit $?
