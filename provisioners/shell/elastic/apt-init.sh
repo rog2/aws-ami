9170531b527477f3d8d764a5631bcf75207ce3bd
zhugongbo <zhugongbo@kingsoft.com>
2017-11-15 19:30:43 +0800

    install ELK 6.x + x-pack + nginx

--- provisioners/shell/elastic/apt-init.sh
+++ provisioners/shell/elastic/apt-init.sh
@@ -6,7 +6,6 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 sudo apt-get update
 sudo apt-get install apt-transport-https
 
-#Save the repository definition to /etc/apt/sources.list.d/elastic-5.x.list
-echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
-
+#Save the repository definition to /etc/apt/sources.list.d/elastic-6.x.list
+echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
 sudo apt-get update
