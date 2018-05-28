97835bbf26168553433ff1d8c4ee4a8208508640
luyue0821 <luyue1@kingsoft.com>
2018-05-28 15:14:21 +0800

    Fix chrony source config error issue.

--- provisioners/shell/chrony.sh
+++ provisioners/shell/chrony.sh
@@ -8,12 +8,8 @@ apt install chrony -y
 
 echo "Configuring Amazon Time Sync Service..."
 CONF_FILE=/etc/chrony/chrony.conf
-TEMP_FILE=/etc/chrony/chrony.conf.tmp
 
-tac $CONF_FILE > $TEMP_FILE
-sed -i '0,/^\(server\|pool\).*/s/^\(server\|pool\).*/server 169.254.169.123 prefer iburst\n&/' $TEMP_FILE
-tac $TEMP_FILE > $CONF_FILE
-rm -f $TEMP_FILE
+sed -i '0,/^\(server\|pool\).*/s/^\(server\|pool\).*/server 169.254.169.123 prefer iburst\n&/' $CONF_FILE
 
 echo "Restarting Amazon Time Sync Service..."
 /etc/init.d/chrony restart
