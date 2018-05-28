ac4174e50754cc2abbfc58b88e18a999870cb06b
luyue0821 <luyue1@kingsoft.com>
2018-05-28 20:15:40 +0800

    Disable periodic apt update and upgrade.

--- provisioners/shell/apt-daily-conf.sh
+++ provisioners/shell/apt-daily-conf.sh
@@ -1,9 +1,20 @@
 #!/bin/bash -ex
 
-conf_path=/etc/systemd/system/apt-daily.timer.d
-conf_file=$conf_path/boot.conf
+# conf_path=/etc/systemd/system/apt-daily.timer.d
+# conf_file=$conf_path/boot.conf
 
-sudo mkdir -p "$conf_path"
-echo "[Timer]" | sudo tee "$conf_file"
-echo "Persistent=false" | sudo tee "$conf_file" -a
-echo "OnBootSec=1h" | sudo tee "$conf_file" -a
+# sudo mkdir -p "$conf_path"
+# echo "[Timer]" | sudo tee "$conf_file"
+# echo "Persistent=false" | sudo tee "$conf_file" -a
+# echo "OnBootSec=1h" | sudo tee "$conf_file" -a
+
+APT_PERIODIC_CONF_PATH=/etc/apt/apt.conf.d
+APT_PERIODIC_CONF_FILE=$APT_PERIODIC_CONF_PATH/99periodic
+
+# Disable apt-daily and apt-daily-upgrade service
+sudo systemctl disable apt-daily.timer
+sudo systemctl disable apt-daily-upgrade.timer
+
+# Disable the update/upgrade script
+sudo mkdir -p "$APT_PERIODIC_CONF_PATH"
+echo 'APT::Periodic::Enable "0"' | sudo tee "$APT_PERIODIC_CONF_FILE"
