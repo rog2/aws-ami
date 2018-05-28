b05e58958ff498ba31c8ea104c4db13e8de03652
luyue0821 <luyue1@kingsoft.com>
2018-05-28 17:28:00 +0800

    Use stepping instead of slewing strategy to adjust system clock at boot time if time diff beyond 5 seconds.

--- provisioners/shell/chrony.sh
+++ provisioners/shell/chrony.sh
@@ -2,6 +2,8 @@
 
 # needs run as root.
 
+AMAZON_NTP_IP=169.254.169.123
+
 echo "Install Amazon Time Sync Service..."
 # http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/set-time.html
 apt install chrony -y
@@ -9,7 +11,9 @@ apt install chrony -y
 echo "Configuring Amazon Time Sync Service..."
 CONF_FILE=/etc/chrony/chrony.conf
 
-sed -i '0,/^\(server\|pool\).*/s/^\(server\|pool\).*/server 169.254.169.123 prefer iburst\n&/' $CONF_FILE
+sed -i '0,/^\(server\|pool\).*/s/^\(server\|pool\).*/server '"$AMAZON_NTP_IP"' prefer iburst\n&/' $CONF_FILE
+
+echo "initstepslew 5 $AMAZON_NTP_IP" >> $CONF_FILE
 
 echo "Restarting Amazon Time Sync Service..."
 /etc/init.d/chrony restart
@@ -17,5 +21,5 @@ echo "Restarting Amazon Time Sync Service..."
 echo "Verifying Amazon Time Sync Service..."
 # waiting until source being determined
 sleep 10
-chronyc tracking |grep 169.254.169.123 &> /dev/null
+chronyc tracking |grep $AMAZON_NTP_IP &> /dev/null
 echo "ok"
