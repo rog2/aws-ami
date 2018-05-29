750852615cc0ab6663d4a4df545575ae1e241f2c
luyue0821 <luyue1@kingsoft.com>
2018-05-29 15:14:21 +0800

    Install cloudformation helper script in base ami.

---
+++ provisioners/shell/cfn-helper-scripts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+# Please refer to
+# https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cfn-helper-scripts-reference.html
+
+sudo easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+sudo ln /usr/local/bin/cfn-hup /etc/init.d/
