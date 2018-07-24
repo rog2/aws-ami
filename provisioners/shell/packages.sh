2dfe411cc5586982fba4e67b71f8c50e3074a026
sunfriendli <pengyang_fly@163.com>
2018-07-24 16:35:25 +0800

    noninteractive upgrade

--- provisioners/shell/packages.sh
+++ provisioners/shell/packages.sh
@@ -4,9 +4,9 @@ set -e
 
 # Do software and security update
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold"  install grub-pc
-sudo apt-get update && sudo apt-get upgrade -y
+# sudo apt-get update && sudo apt-get upgrade -y
 # sudo apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold"  update && sudo apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold"  dist-upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 
 # Common packages across all versions
 DEPS="
