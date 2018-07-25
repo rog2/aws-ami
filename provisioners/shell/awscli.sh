1c0201b6077d4ce1328787d632dc277fd4a2210e
sunfriendli <pengyang_fly@163.com>
2018-07-25 10:01:01 +0800

    awscli install with pip3

--- provisioners/shell/awscli.sh
+++ provisioners/shell/awscli.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -ex
 
-sudo apt-get -y install python-pip
-
+# sudo apt-get -y install python-pip
+sudo apt-get install python3-pip
 # Install AWS CLI using pip
 # To upgrade an existing AWS CLI installation, use the --upgrade option:
 #   sudo pip install --upgrade awscli
-sudo -H pip install awscli
+sudo -H pip3 install awscli
 
 # Enable Command Completion for AWS CLI
 echo 'complete -C $(which aws_completer) aws' >> ~/.bash_completion
