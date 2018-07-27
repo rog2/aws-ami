da6dbd749a33c1ec2b2697d824ab45f8083e34de
sunfriendli <pengyang_fly@163.com>
2018-07-27 15:33:54 +0800

    set retrie timeout, remove -y

--- provisioners/shell/cfn-helper-scripts.sh
+++ provisioners/shell/cfn-helper-scripts.sh
@@ -5,8 +5,9 @@
 
 # sudo easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
 # sudo pip -y --timeout=3000 install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
-wget https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+wget -c -t0 -T6000 https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+
 md5sum aws-cfn-bootstrap-latest.tar.gz
-pip install -y aws-cfn-bootstrap-latest.tar.gz
+pip install aws-cfn-bootstrap-latest.tar.gz
 
 sudo ln /usr/local/bin/cfn-hup /etc/init.d/
