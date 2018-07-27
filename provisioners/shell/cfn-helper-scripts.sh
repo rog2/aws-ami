a0e35f76d0b5a7cf83ec291ae5a5a707b98fd804
sunfriendli <pengyang_fly@163.com>
2018-07-27 16:03:45 +0800

    remove params

--- provisioners/shell/cfn-helper-scripts.sh
+++ provisioners/shell/cfn-helper-scripts.sh
@@ -5,7 +5,7 @@
 
 # sudo easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
 # sudo pip -y --timeout=3000 install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
-wget -c -t0 -T6000 https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+wget https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
 
 md5sum aws-cfn-bootstrap-latest.tar.gz
 pip install aws-cfn-bootstrap-latest.tar.gz
