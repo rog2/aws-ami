85afef5e9a7063dfd6a5b7c743fda8d2801767ad
sunfriendli <pengyang_fly@163.com>
2018-07-30 17:30:31 +0800

    change apt-get to pip when install awscli

--- provisioners/shell/awscli.sh
+++ provisioners/shell/awscli.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -ex
 
+sudo apt-get -y install python-pip
+
 # Install AWS CLI using pip
 # To upgrade an existing AWS CLI installation, use the --upgrade option:
 # sudo pip install --upgrade awscli
-sudo apt update
-sudo apt install -y python-pip
+sudo pip install --retries=20 --timeout=3000 awscli
 
-# sudo pip install --no-cache-dir --timeout=3000 awscli
 # sudo apt-get update
-sudo apt-get install -y awscli
+# sudo apt-get install -y awscli
 
 # Enable Command Completion for AWS CLI
 echo 'complete -C $(which aws_completer) aws' >> ~/.bash_completion
