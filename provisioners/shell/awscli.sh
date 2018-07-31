c77be1ed853bf3c202cc2425a7071bbf34ade5d1
sunfriendli <pengyang_fly@163.com>
2018-07-31 11:06:32 +0800

    add domestic mirror

--- provisioners/shell/awscli.sh
+++ provisioners/shell/awscli.sh
@@ -5,7 +5,7 @@ sudo apt-get -y install python-pip
 # Install AWS CLI using pip
 # To upgrade an existing AWS CLI installation, use the --upgrade option:
 # sudo pip install --upgrade awscli
-sudo pip install --retries=20 --timeout=3000 awscli
+sudo pip install awscli -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
 
 # sudo apt-get update
 # sudo apt-get install -y awscli
