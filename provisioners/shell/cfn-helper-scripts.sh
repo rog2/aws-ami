5687710831be38669869571338e0c476768d66a7
sunfriendli <pengyang_fly@163.com>
2018-07-31 14:10:47 +0800

    remove install cfn-bootstrap

--- provisioners/shell/cfn-helper-scripts.sh
+++ provisioners/shell/cfn-helper-scripts.sh
@@ -5,8 +5,10 @@
 
 # sudo easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
 # sudo pip -y --timeout=3000 install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
-wget --tries=75 --timeout=300 https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
-md5sum aws-cfn-bootstrap-latest.tar.gz
-pip install aws-cfn-bootstrap-latest.tar.gz
+
+# ***On the latest Amazon Linux AMI version, the scripts are installed in /opt/aws/bin.***
+#wget --tries=75 --timeout=300 https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+#md5sum aws-cfn-bootstrap-latest.tar.gz
+#pip install aws-cfn-bootstrap-latest.tar.gz
 
 # sudo ln /usr/local/bin/cfn-hup /etc/init.d/
