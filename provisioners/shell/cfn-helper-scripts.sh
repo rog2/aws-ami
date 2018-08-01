83e0c18007101ca5117c45dad99ab2b339e1715e
sunfriendli <pengyang_fly@163.com>
2018-08-01 16:39:24 +0800

    change source region of tar.gz to cn

--- provisioners/shell/cfn-helper-scripts.sh
+++ provisioners/shell/cfn-helper-scripts.sh
@@ -3,16 +3,9 @@
 # Please refer to
 # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cfn-helper-scripts-reference.html
 # sudo easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+# 中国区各个服务的 Endpoint:http://docs.amazonaws.cn/en_us/general/latest/gr/rande.html
 
-# lipengyang 
-# /usr/bin/easy_install was not included in the dist-packages of python-setuptools in bionic LTS,which was included in xenial LTS,so I can't use easy_install directly 
-# https://packages.ubuntu.com/bionic/all/python-setuptools/filelist
-# https://packages.ubuntu.com/xenial/all/python-setuptools/filelist
+sudo apt-get install python-setuptools
+sudo python -m easy_install https://s3.cn-north-1.amazonaws.com.cn/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
 
-
-# sudo apt-get install python-setuptools
-# sudo wget https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
-# sudo python /usr/lib/python2.7/dist-packages/easy_install.py aws-cfn-bootstrap-latest.tar.gz
-# sudo rm -f aws-cfn-bootstrap-latest.tar.gz
-
-# sudo ln /usr/local/bin/cfn-hup /etc/init.d/
+sudo ln /usr/local/bin/cfn-hup /etc/init.d/
