1006851b9b287dc6e650fa178494687008f1b0e4
sunfriendli <pengyang_fly@163.com>
2018-08-16 19:46:58 +0800

    Update relate config to match ubuntu 18.04.

--- provisioners/shell/awscli.sh
+++ provisioners/shell/awscli.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -ex
 
-sudo apt-get -y install python-pip
+sudo apt-get -y install python3-pip
 
-# Install AWS CLI using pip
-# To upgrade an existing AWS CLI installation, use the --upgrade option:
-#   sudo pip install --upgrade awscli
-sudo pip install awscli
+# Install AWS CLI using pip3
+# To upgrade an existing AWS CLI installation, use the --upgrade option: sudo pip3 install --upgrade awscli
+sudo pip3 install awscli
 
 # Enable Command Completion for AWS CLI
 echo 'complete -C $(which aws_completer) aws' >> ~/.bash_completion
