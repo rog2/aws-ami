174e5f69fd9afd61ce149e8779edeb912bcb9c31
sunfriendli <pengyang_fly@163.com>
2018-08-17 15:29:44 +0800

    Cancel upgrade and ancel support ubuntu version less than 18.04 LTS.

--- provisioners/shell/packages.sh
+++ provisioners/shell/packages.sh
@@ -3,11 +3,6 @@
 set -e
 
 sudo apt-get update
-# https://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt?answertab=active#tab-top
-# the default answer is “keep the existing file”
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-
-# remove realpath, and coreutils instead in ubuntu 18.04
 
 # Common packages across all versions
 DEPS="
@@ -21,29 +16,8 @@ DEPS="
     jq
     aria2
     gdb
-    "
-
-# Version specific packages
-DEPS_TRUSTY="
-    ruby2.0
-    "
-DEPS_XENIAL="
-    ruby2.3
-    "
-# https://packages.ubuntu.com/bionic/ruby
-DEPS_BIONIC="
     ruby2.5
     "
-
-CODENAME=$(lsb_release -s -c)
-
-if [ "$CODENAME" = "xenial" ]; then
-    DEPS="$DEPS $DEPS_XENIAL"
-elif [ "$CODENAME" = "bionic" ]; then
-    DEPS="$DEPS $DEPS_BIONIC"
-else
-    DEPS="$DEPS $DEPS_TRUSTY"
-fi
 
 # Install basic packages
 for dep in $DEPS; do
