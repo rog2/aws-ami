184b3b4c35963316b169515f0cc4c1a4554631d4
李朋洋 <1040868321@qq.com>
2018-08-27 14:12:24 +0800

    Change shells and configurations to be used to build ami base on ubuntu 18.04. (#16)

--- provisioners/shell/packages.sh
+++ provisioners/shell/packages.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-# Do software and security update
-sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get update
 
 # Common packages across all versions
 DEPS="
@@ -12,28 +11,13 @@ DEPS="
     unrar
     htop
     ifstat
-    realpath
+    coreutils
     tree
     jq
     aria2
     gdb
+    ruby2.5
     "
-
-# Version specific packages
-DEPS_TRUSTY="
-    ruby2.0
-    "
-DEPS_XENIAL="
-    ruby2.3
-    "
-
-CODENAME=$(lsb_release -s -c)
-
-if [ "$CODENAME" = "xenial" ]; then
-    DEPS="$DEPS $DEPS_XENIAL"
-else
-    DEPS="$DEPS $DEPS_TRUSTY"
-fi
 
 # Install basic packages
 for dep in $DEPS; do
---
+++ provisioners/shell/pip-source.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+# Depends on pip.conf has been copyed to /tmp/pip
+if [[ $1 == cn-* ]]; then
+    sudo mkdir ~/.pip
+    sudo chmod -R a+w ~/.pip
+    sudo cp -vf /tmp/pip/pip.conf ~/.pip/pip.conf
+fi
