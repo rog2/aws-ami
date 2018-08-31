e5ef7fe6dc05d45355ffe570e5fba4483cf30180
luyue0821 <luyue1@kingsoft.com>
2018-08-31 11:19:45 +0800

    Install salt with python3.

---
+++ provisioners/shell/salt/salt-master.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+# https://repo.saltstack.com/#ubuntu
+
+# Depends on salt-repo has been configurated.
+
+sudo apt update && sudo apt install salt-master salt-api salt-cloud -y
+
+echo 'Disable salt-master & salt-api...'
+sudo systemctl disable salt-master.service
+sudo systemctl disable salt-api.service
+
+echo 'Salt installed.'
+salt --versions-report
