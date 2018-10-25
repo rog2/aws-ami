e76a1bb7f5a9e1cef7c8d7f1a341641f5be545bf
liukch <liukangcheng@kingsoft.com>
2018-10-25 17:10:19 +0800

    Change java11-openjdk.sh. Fix bug: systemd can't read PATH from profile.

--- provisioners/shell/java11-openjdk.sh
+++ provisioners/shell/java11-openjdk.sh
@@ -5,20 +5,19 @@ SOURCE_URL=https://download.java.net/java/GA/jdk11/13/GPL/openjdk-"$JAVA_VERSION
 SOURCE_FILE=openjdk-"$JAVA_VERSION"_linux-x64_bin.tar.gz
 SOURCE_FOLDER_NAME=jdk-$JAVA_VERSION
 
-PROFILE_FILE=/etc/profile.d/openjdk-11.sh
-
 cd /tmp
 if [ ! -e $SOURCE_FILE ]; then
     wget $SOURCE_URL
 fi
 tar -zxf $SOURCE_FILE
 mkdir -p /opt/java/$SOURCE_FOLDER_NAME
-sudo mv $SOURCE_FOLDER_NAME /opt/java/
+sudo mv -f $SOURCE_FOLDER_NAME/* /opt/java/$SOURCE_FOLDER_NAME/
 
-sudo touch $PROFILE_FILE
-sudo chmod 644 $PROFILE_FILE
+if [ ! -d /usr/local/bin ]; then
+    sudo mkdir -p /usr/local/bin
+fi
 
-sudo tee "$PROFILE_FILE" << EOF
-export JAVA_HOME=/opt/java/jdk-$JAVA_VERSION
-export PATH=\$JAVA_HOME/bin:\$PATH
-EOF
+for bin in /opt/java/$SOURCE_FOLDER_NAME/bin/*; do
+    sudo update-alternatives --install /usr/local/bin/$(basename $bin) $(basename $bin) $bin 100;
+    sudo update-alternatives --set $(basename $bin) $bin;
+done
