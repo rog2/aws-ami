1eb7ce24a9a67cefabdfc0060c4e27d17567a7f1
李朋洋 <1040868321@qq.com>
2018-11-07 19:05:04 +0800

    Config cn pip source in script derectly and cancel .pip authority set. (#33)

--- provisioners/shell/pip-source.sh
+++ provisioners/shell/pip-source.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -e
 
-# Depends on pip.conf has been copyed to /tmp/pip
-if [[ $1 == cn-* ]]; then
-    mkdir ~/.pip
-    chmod 700 ~/.pip
-    cp -vf /tmp/pip/pip.conf ~/.pip/pip.conf
+az=$(ec2metadata --availability-zone)
+if [[ $az != cn-* ]]; then 
+    exit 0
 fi
+
+mkdir ~/.pip
+
+# Config cn pip source
+tee ~/.pip/pip.conf << EOF
+[global]
+index-url = https://pypi.tuna.tsinghua.edu.cn/simple
+EOF
