6abc48b79d8c7e08674bfded13a19b04d2d6eba8
Zhiguang Wang <wangzhiguang@kingsoft.com>
2018-11-16 12:08:26 +0800

    Move pip upgrade code out of packages.sh

--- provisioners/shell/packages.sh
+++ provisioners/shell/packages.sh
@@ -31,7 +31,3 @@ for dep in $DEPS; do
     set +x
     fi
 done
-
-# Upgrade packages on demand
-echo "Upgrading package: pip"
-sudo python3 -m pip install --upgrade pip
--- provisioners/shell/pip-source.sh
+++ provisioners/shell/pip-source.sh
@@ -1,14 +1,17 @@
-#!/bin/bash -e
+#!/bin/bash
 
-az=$(ec2metadata --availability-zone)
-if [[ $az != cn-* ]]; then 
-    exit 0
-fi
+set -e
 
-mkdir ~/.pip
-
-# Config cn pip source
-tee ~/.pip/pip.conf << EOF
+# Use pip mirror from tsinghua university if we are in China region,
+# This works around network connectivity issue caused by the GFW.
+local readonly az=$(ec2metadata --availability-zone)
+if [[ $az == cn-* ]]; then
+  mkdir -p ~/.pip
+  tee ~/.pip/pip.conf << EOF
 [global]
 index-url = https://pypi.tuna.tsinghua.edu.cn/simple
 EOF
+fi
+
+echo "Upgrading pip:"
+sudo python3 -m pip install --upgrade pip
