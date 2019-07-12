f00e0bc65c9a5cc249cb14f04e125a0d4869c9f6
Zhiguang Wang <wangzhiguang@kingsoft.com>
2019-07-12 10:15:04 +0800

    Add kernel tuning script.

---
+++ provisioners/shell/kernel-tuning.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+readonly SYSCTL_CONF="/etc/sysctl.d/98-seasungames-kernel-tuning.conf"
+
+sudo tee "$SYSCTL_CONF" <<EOF
+# Disable virtual memory
+vm.swappiness = 0
+
+# Networking
+net.core.somaxconn = 1024
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+EOF
+
+sudo sysctl -p "$SYSCTL_CONF"
