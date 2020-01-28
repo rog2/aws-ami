c8bc1777f40cb0ebf3f27d7b341d3620f11e5d5d
Zhiguang Wang <wangzhiguang@kingsoft.com>
2020-01-28 17:58:40 +0800

    Upgrade Amazon Corretto to 11.0.6

--- provisioners/shell/java-11-amazon-corretto.sh
+++ provisioners/shell/java-11-amazon-corretto.sh
@@ -4,12 +4,13 @@ set -e
 
 source "$BASH_HELPERS"
 
-# See: https://docs.aws.amazon.com/corretto/latest/corretto-11-ug/generic-linux-install.html
+# https://github.com/corretto/corretto-11/releases
+# https://docs.aws.amazon.com/corretto/latest/corretto-11-ug/downloads-list.html
 function install {
   assert_not_empty "JAVA_VERSION" "$JAVA_VERSION"
   local readonly version="$JAVA_VERSION"
   local readonly arch=$(dpkg --print-architecture)
-  local readonly download_url="https://d3pxv6yz143wms.cloudfront.net/${version/-/.}/java-11-amazon-corretto-jdk_${version}_${arch}.deb"
+  local readonly download_url="https://corretto.aws/downloads/resources/${version/-/.}/java-11-amazon-corretto-jdk_${version}_${arch}.deb"
   local readonly download_path="/tmp/$(basename "$download_url")"
   http_download "$download_url" "$download_path"
   sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -q java-common
