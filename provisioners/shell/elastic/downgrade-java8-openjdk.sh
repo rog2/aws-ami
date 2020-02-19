87fe721f74cfbcb712d00279989b6aa45186502d
Zhiguang Wang <wangzhiguang@kingsoft.com>
2020-02-19 10:48:18 +0800

    Delete deprecated templates/conf/shell.

--- provisioners/shell/elastic/downgrade-java8-openjdk.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ex
-sudo apt update
-
-sudo apt -y install openjdk-8-jdk
-sudo apt -y install icedtea-8-plugin
-sudo update-java-alternatives --set /usr/lib/jvm/java-1.8.0-openjdk-amd64
