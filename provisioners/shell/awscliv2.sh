6c585d9cc749c168f19d06638dd6775827251841
Zhiguang Wang <wangzhiguang@kingsoft.com>
2021-01-26 23:24:08 +0800

    awscliv2: Use S3 file mirror to download installer.

--- provisioners/shell/awscliv2.sh
+++ provisioners/shell/awscliv2.sh
@@ -2,13 +2,15 @@
 
 set -e
 
+source "$BASH_HELPERS"
+
 readonly DOWNLOAD_URL="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
 readonly LOCAL_PATH="/tmp/awscliv2.zip"
 
 # https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
 function install_awscliv2 {
   echo "Downloading awscliv2 from $DOWNLOAD_URL"
-  curl -o "$LOCAL_PATH" "$DOWNLOAD_URL" --location --silent --fail --show-error
+  http_download "$DOWNLOAD_URL" "$LOCAL_PATH"
   pushd "$(dirname "$LOCAL_PATH")"
     unzip -q "$LOCAL_PATH"
     sudo ./aws/install
