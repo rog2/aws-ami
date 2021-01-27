ae133d4199c6d759bf43285753a023817b9b3fcb
Zhiguang Wang <wangzhiguang@kingsoft.com>
2021-01-27 09:09:54 +0800

    Revert "awscliv2: Use S3 file mirror to download installer."
    This reverts commit 6c585d9cc749c168f19d06638dd6775827251841.

--- provisioners/shell/awscliv2.sh
+++ provisioners/shell/awscliv2.sh
@@ -2,15 +2,13 @@
 
 set -e
 
-source "$BASH_HELPERS"
-
 readonly DOWNLOAD_URL="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
 readonly LOCAL_PATH="/tmp/awscliv2.zip"
 
 # https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
 function install_awscliv2 {
   echo "Downloading awscliv2 from $DOWNLOAD_URL"
-  http_download "$DOWNLOAD_URL" "$LOCAL_PATH"
+  curl -o "$LOCAL_PATH" "$DOWNLOAD_URL" --location --silent --fail --show-error
   pushd "$(dirname "$LOCAL_PATH")"
     unzip -q "$LOCAL_PATH"
     sudo ./aws/install
