bee2d9de9fa99b2ca61d9fd239641e4a1b588dc9
Zhiguang Wang <wangzhiguang@kingsoft.com>
2021-01-31 10:58:24 +0800

    apt-mirrors: Add support for arm64.

--- provisioners/shell/apt-mirrors.sh
+++ provisioners/shell/apt-mirrors.sh
@@ -11,21 +11,32 @@
 
 set -e
 
+function get_repo_name {
+  local readonly arch=$(dpkg --print-architecture)
+  # https://developer.aliyun.com/mirror/ubuntu-ports
+  if [[ "$arch" == "arm64" ]]; then
+    echo "ubuntu-ports"
+  else
+    echo "ubuntu"
+  fi
+}
+
 function change_apt_sources {
   local readonly codename=$(lsb_release -cs)
+  local readonly repo=$(get_repo_name)
   echo 'Changing apt sources to mirrors.aliyun.com'
   sudo mv /etc/apt/sources.list /etc/apt/sources.list.original
   sudo tee /etc/apt/sources.list << EOF
-deb http://mirrors.aliyun.com/ubuntu/ ${codename} main restricted
-deb http://mirrors.aliyun.com/ubuntu/ ${codename}-updates main restricted
-deb http://mirrors.aliyun.com/ubuntu/ ${codename} universe
-deb http://mirrors.aliyun.com/ubuntu/ ${codename}-updates universe
-deb http://mirrors.aliyun.com/ubuntu/ ${codename} multiverse
-deb http://mirrors.aliyun.com/ubuntu/ ${codename}-updates multiverse
-deb http://mirrors.aliyun.com/ubuntu/ ${codename}-backports main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ ${codename}-security main restricted
-deb http://mirrors.aliyun.com/ubuntu/ ${codename}-security universe
-deb http://mirrors.aliyun.com/ubuntu/ ${codename}-security multiverse
+deb http://mirrors.aliyun.com/${repo}/ ${codename} main restricted
+deb http://mirrors.aliyun.com/${repo}/ ${codename}-updates main restricted
+deb http://mirrors.aliyun.com/${repo}/ ${codename} universe
+deb http://mirrors.aliyun.com/${repo}/ ${codename}-updates universe
+deb http://mirrors.aliyun.com/${repo}/ ${codename} multiverse
+deb http://mirrors.aliyun.com/${repo}/ ${codename}-updates multiverse
+deb http://mirrors.aliyun.com/${repo}/ ${codename}-backports main restricted universe multiverse
+deb http://mirrors.aliyun.com/${repo}/ ${codename}-security main restricted
+deb http://mirrors.aliyun.com/${repo}/ ${codename}-security universe
+deb http://mirrors.aliyun.com/${repo}/ ${codename}-security multiverse
 EOF
 }
 
