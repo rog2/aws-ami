16861100b1dd1a90d01b24fa0ee186eb2519e3bb
Zhiguang Wang <wangzhiguang@kingsoft.com>
2021-06-21 21:41:56 +0800

    kernel-tuning: net.ipv4.ip_local_port_range = 1024 65535

--- provisioners/kernel-tuning.sh
+++ provisioners/kernel-tuning.sh
@@ -9,9 +9,13 @@ sudo tee "$SYSCTL_CONF" <<EOF
 vm.swappiness = 0
 
 # Networking
+# upper limit allowed for a listen() backlog
 net.core.somaxconn = 1024
+# per-socket receive/send buffers
 net.core.rmem_max = 16777216
 net.core.wmem_max = 16777216
+# port range used by TCP and UDP to choose the local port
+net.ipv4.ip_local_port_range = 1024 65535
 EOF
 
 sudo sysctl -p "$SYSCTL_CONF"
